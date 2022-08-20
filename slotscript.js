bb7cd02605d3793eb3ebcced5e2f32e1dd2cba21
matthewberes <matthewberes@trentu.ca>
2022-08-20 12:54:27 -0400

    bug fix and clean up
    forgot to run updateBal() when you win while logged in, database was not updating with the earnings,
    
    commenting, and cleaning/shrinking code today

--- slotscript.js
+++ slotscript.js
@@ -2,7 +2,7 @@
 //simple slot machine
 
 //works with a mysql database named "slots" and a table named "logininfo"
-//5 columns in the table: int usersId, varchar usersName, varchar usersPwd, int usersBal, usersDate
+//5 columns in the table: int usersId, varchar usersName, varchar usersPwd, int usersBal, varchar usersDate
 
 //deposit and withdraw funds
 //generates 3 random numbers with a range of 0-5 and determining if all 3 numbers are equal
@@ -52,11 +52,10 @@ $(document).ready(function() {
 
 	Deposit.addEventListener("click", (d) => {
 		d.preventDefault();
-
+		//prevent deposit button from being clicked if user failed log in
 		if (logInError > 0){
 			return
 		}
-
 		var input = document.getElementById("inputNum").value;
 		if (input > 0){
 			total = +total + +input;
@@ -66,13 +65,11 @@ $(document).ready(function() {
 			let html = "<span style='color:green'> +" + input + "</span><br>";
 			p.insertAdjacentHTML('afterbegin', html);
 
-
 			//update database
 			if (loggedIn > 0){
 				updateBal();
 			}
 		}
-
 	})
 
 	//withdraw
@@ -80,11 +77,10 @@ $(document).ready(function() {
 	
 	Withdraw.addEventListener("click", (w) => {
 		w.preventDefault();
-
+		//prevent withdraw button from being clicked if user failed log in
 		if (logInError > 0){
 			return
 		}
-
 		var input = document.getElementById("inputNum").value;
 		if (input <= total && input > 0){
 			total = +total - +input;
@@ -109,7 +105,6 @@ $(document).ready(function() {
 	//raise bet
 	document.getElementById("add1").onclick = function() {add1()};
 	function add1() {
-		
 		if (bet > -1){
 			bet++;
 			document.getElementById("curBet").innerHTML = "Bet: " + bet;
@@ -119,7 +114,6 @@ $(document).ready(function() {
 	//lower bet
 	document.getElementById("sub1").onclick = function() {sub1()};
 	function sub1() {
-		
 		if (bet != 0){
 			bet--;
 			document.getElementById("curBet").innerHTML = "Bet: " + bet;
@@ -131,7 +125,6 @@ $(document).ready(function() {
 	//spin
 	document.getElementById("pull").onclick = function() {RNG()};
 	function RNG() {
-
 		if (bet > total){
 			alert("Insufficient Funds");
 		}
@@ -153,30 +146,36 @@ $(document).ready(function() {
 			var num2 = Math.floor(Math.random() * 6);
 			var num3 = Math.floor(Math.random() * 6);
 
-			//DEBUG NEXT SLOTS AT START
-			//console.log("num1 " + num1)
-			//console.log("num2 " + num2)
-			//console.log("num3 " + num3)
 			
 			//calculate distance
 			var dist1 = distanceCalc(lastSlot1, num1);
 			var dist2 = distanceCalc(lastSlot2, num2);
 			var dist3 = distanceCalc(lastSlot3, num3);
 
+			//keep track of previous slot for distanceCalc
+			lastSlot1 = num1;
+			lastSlot2 = num2;
+			lastSlot3 = num3;
+
+			//animate slots
+			spinAnimation(dist1, num1, lastSlot1, 1, null, null);
+			spinAnimation(dist2, num2, lastSlot2, 2, null, null);
+			spinAnimation(dist3, num3, lastSlot3, 3, num1, num2);
+			
 			//DEBUG SLOT DISTANCE
 			//console.log("dist1 = "+ dist1)
 			//console.log("dist2 = "+ dist2)
 			//console.log("dist3 = "+ dist3)
 
-			//animate slots
-			spinAnimation1(dist1, num1, lastSlot1);
-			spinAnimation2(dist2, num2, lastSlot2);
-			spinAnimation3(dist3, num3, lastSlot3, num1, num2);
-			
-			//keep track of previous slot for distanceCalc
-			lastSlot1 = num1;
-			lastSlot2 = num2;
-			lastSlot3 = num3;
+			//DEBUG NEXT SLOTS AT START
+			//console.log("num1 " + num1)
+			//console.log("num2 " + num2)
+			//console.log("num3 " + num3)
+
+			//DEBUG LAST SLOTS AT START
+			//console.log("last1 " + lastSlot1)
+			//console.log("last2 " + lastSlot2)
+			//console.log("last3 " + lastSlot3)
 		}
 	}
 
@@ -227,6 +226,7 @@ $(document).ready(function() {
 	//log out
 	document.getElementById("logOutButton").onclick = function() {LO()};
 	function LO() {
+		//display log in page
 		var l = document.getElementById("logInPage");
 		var s = document.getElementById("signUpPage");
 		var o = document.getElementById("loggedInPage");
@@ -235,12 +235,16 @@ $(document).ready(function() {
 		s.style.display = "none";
 		o.style.display = "none";
 
+		//reset values
 		total = 69;
+		bet = 10;
 		loggedIn = 0;
 		logInError = 0;
 
+		document.getElementById("passwordL").value = "";
 		document.getElementById("logInTitle").innerHTML = "Log in";
 		document.getElementById("totalNum").innerHTML = "Total: " + total;
+		document.getElementById("curBet").innerHTML = "Bet: " + bet;
 		document.getElementById("dynamicPara").innerHTML = "";
 	}
 
@@ -252,19 +256,12 @@ $(document).ready(function() {
 
 		but.value = "Log out";
 		document.getElementById("userSince").style.display = "block";
+		bet = 10;
+		document.getElementById("curBet").innerHTML = "Bet: " + bet;
 
 		var userName = document.getElementById("usernameL").value;
 		var passWord = document.getElementById("passwordL").value;
-
-		//check if inputs are empty
-		if (typeof userName === 'string' && userName.length === 0 || typeof passWord === 'string' && passWord.length === 0 ){
-			document.getElementById("error").innerHTML = "Empty input";
-			document.getElementById("error").style.color = "red";
-			document.getElementById("error").style.display = "block";
-			document.getElementById("userSince").style.display = "none";
-			document.getElementById('logOutButton').value = "Try again";
-		}
-
+		
 		//updates logInTitle
 		$.ajax({
 			url: "usernameAJAX.php",
@@ -278,7 +275,7 @@ $(document).ready(function() {
 			}
 		});
 
-		//updates balance and profile display
+		//updates balance display
 		$.ajax({
 			url: "balanceAJAX.php",
 			method: "post",
@@ -307,10 +304,24 @@ $(document).ready(function() {
 				var res = str.replace(/\D/g, "");
 				total = +res
 				
-				//allows database updates from updateBal()
+				//allows for database updates from updateBal()
 				loggedIn = 1;							
 			}
 		});
+
+		//updates balance in database
+		function updateBal(){
+			var userName = document.getElementById("usernameL").value;
+
+			$.ajax({
+				url: "updateAJAX.php",
+				method: "post",
+				data:{usersName: userName, usersBal: total},
+				dataType: "text",
+				success: function(data){
+				}
+			});
+		}
 
 		//updates userSince
 		$.ajax({
@@ -326,21 +337,7 @@ $(document).ready(function() {
 		});
 	}
 
-	//if user is signed in, this keeps track of balance
-	function updateBal(){
-		var userName = document.getElementById("usernameL").value;
-
-		$.ajax({
-			url: "updateAJAX.php",
-			method: "post",
-			data:{usersName: userName, usersBal: total},
-			dataType: "text",
-			success: function(data){
-			}
-		});
-	}
-
-	//remove error msgs
+	//remove login error msgs
 	document.getElementById("username").onclick = function() {removeError()};
 	function removeError(){
 		var e = document.getElementById("error");
@@ -348,63 +345,35 @@ $(document).ready(function() {
 	}
 
 	document.getElementById("usernameL").onclick = function() {removeError()};
-
 	document.getElementById("password").onclick = function() {removeError()};
-
 	document.getElementById("passwordL").onclick = function() {removeError()};
 
-	//spin slot 1 proper amount
-	async function spinAnimation1(slot1Distance, slot1Destination, slotStart1){
-		
+	//functions
+
+	//spin slots proper amount
+	async function spinAnimation(slotDistance, slotDestination, slotStart, n, num1, num2){
+		let num = n;
+		let spins = n *= 6;
+
 		//for each number from previous slot to the next slot + 1 full spin
-		for (let i = 0; i <= slot1Distance + 6; i++){
-			nextSlot = (slotStart1 + i) % 6;
+		for (let i = 0; i <= slotDistance + spins; i++){
+			nextSlot = (slotStart + i) % 6;
 			nextImage = nextSlot + ".png";
+
+			//setting up variable name for slot image
+			var varName = "slot" + num;
 
 			//setting up initial images
-			document.getElementById("slot1").src = "img/" + nextImage;
+			document.getElementById(varName).src = "img/" + nextImage;
 			await sleep(60);
-
-			//DEBUG IMAGES
-			//console.log("Image 1 = " + nextImage);
-		}
-	}
-
-	//spin slot 2 proper amount
-	async function spinAnimation2(slot2Distance, slot2Destination, slotStart2){
-		
-		//for each number from previous slot to the next slot + 2 full spins
-		for (let i = 0; i <= slot2Distance + 12; i++){
-			nextSlot = (slotStart2 + i) % 6;
-			nextImage = nextSlot + ".png";
-
-			//setting up initial images in loop
-			document.getElementById("slot2").src = "img/" + nextImage;
-			await sleep(60);
-
-			//DEBUG IMAGES
-			//console.log("Image 2 = " + nextImage);
-		}
-	}
-
-	//spin slot 3 proper amount
-	async function spinAnimation3(slot3Distance, slot3Destination, slotStart3, num1, num2){
-		
-		//for each number from previous slot to the next slot + 3 full spins
-		for (let i = 0; i <= slot3Distance + 18; i++){
-			nextSlot = (slotStart3 + i) % 6;
-			nextImage = nextSlot + ".png";
-
-			//setting up initial images
-			document.getElementById("slot3").src = "img/" + nextImage;
-			await sleep(60);
-
-			//DEBUG IMAGES
-			//console.log("Image 3 = " + nextImage);
-
 		}
 		//see if you win
-		calcWinnings(num1, num2, slot3Destination);
+		if (num == 3){
+			calcWinnings(num1, num2, slotDestination);
+		}
+
+		//DEBUG IMAGES
+		//console.log("Image = " + nextImage);
 	}
 
 	//wait function
@@ -415,38 +384,40 @@ $(document).ready(function() {
 	//calculate distance from last slot to new slot
 	function distanceCalc(lastSlot, nextSlot){
 		var distance;
+		distance = (6 - lastSlot + nextSlot) % 6;
+		return distance;
 
 		//DEBUG DISTANCE CALCULATION
 		//console.log("lastSlot = " + lastSlot);
 		//console.log("nextSlot = " + nextSlot);
-
-		distance = (6 - lastSlot + nextSlot) % 6;
-		return distance;
-
 	}
 
 	//check if all 3 numbers are equal and delivers reward
 	function calcWinnings(num1, num2, num3){
-			
+		//if all 3 numbers are the same
+		if (num1 == num2 && num1 == num3){
+			console.log("you won")
+				
+			//calculate winnings
+			var multi = +num1 + 1;
+			var winnings = +bet * +multi;
+
+			//display win message
+			alert("You won: " + winnings);
+
+			//update balance
+			total = +total + +winnings
+			document.getElementById("totalNum").innerHTML = "Total: " + total;
+
+			//update database
+			if (loggedIn > 0){
+				updateBal();
+			}
+		}	
+
 		//DEBUG SLOTS AT END
 		console.log("slot 1 = "+num1)
 		console.log("slot 2 = "+num2)
-		console.log("slot 3 = "+num3)
-
-		//if all 3 numbers are the same
-		if (num1 == num2 && num1 == num3){
-		console.log("you won")
-			
-		//calculate winnings
-		var multi = +num1 + 1;
-		var winnings = +bet * +multi;
-
-		//display win message
-		alert("You won: " + winnings);
-
-		//update balance
-		total = +total + +winnings
-		document.getElementById("totalNum").innerHTML = "Total: " + total;
-		}		
+		console.log("slot 3 = "+num3)	
 	}
 });
